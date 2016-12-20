ceba75b9220d1f72793fee2c44435d99ccc48f76
Joe Maples <joe@frap129.org>
2016-12-20 19:14:10 +0000

    adreno_idler: Add display state awareness
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/devfreq/adreno_idler.c
+++ drivers/devfreq/adreno_idler.c
@@ -75,7 +75,12 @@ int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 	if (!adreno_idler_active)
 		return 0;
 
+<<<<<<< HEAD
 	display_on = is_display_on();
+=======
+	/* Boolean to let us know if the display is on*/
+	bool display_on = is_display_on();
+>>>>>>> 79e71e1... adreno_idler: Add display state awareness
 
 	if (stats.busy_time < idleworkload) {
 		/* busy_time >= idleworkload should be considered as a non-idle workload. */
