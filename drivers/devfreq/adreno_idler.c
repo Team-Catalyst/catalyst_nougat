af9beb176e6c937a99a93721b148a80eeae28edf
Joe Maples <joe@frap129.org>
2016-12-25 12:12:47 -0500

    adreno_idler: Declare display_on correctly
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/devfreq/adreno_idler.c
+++ drivers/devfreq/adreno_idler.c
@@ -74,14 +74,16 @@ static inline int64_t get_time_inms(void) {
 }
 static int64_t idle_lasttime = 0;
 
+/* Boolean to let us know if the display is on*/
+static bool display_on;
+
 int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 		 unsigned long *freq)
 {
 	if (!adreno_idler_active)
 		return 0;
 
-	/* Boolean to let us know if the display is on*/
-	bool display_on = is_display_on();
+	display_on = is_display_on();
 
 	if (stats.busy_time < idleworkload) {
 		/* busy_time >= idleworkload should be considered as a non-idle workload. */
